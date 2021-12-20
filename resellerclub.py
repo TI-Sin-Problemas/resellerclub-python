18f05f7e2090f6cd2f11d40d8427cf026b780c26
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2021-12-19 20:36:02 -0600

    Add required private methods

--- resellerclub.py
+++ resellerclub.py
@@ -1,4 +1,6 @@
-from .urls import URLs
+from xml.etree import ElementTree
+import requests
+from urls import URLs
 
 
 class ResellerClubAPI:
@@ -7,11 +9,32 @@ class ResellerClubAPI:
     def __init__(
         self,
         auth_userid: str,
-        token: str,
+        api_key: str,
         response_format: str = "json",
         debug: bool = True,
     ) -> None:
         self.auth_userid = auth_userid
-        self.token = token
+        self.api_key = api_key
         self.response_format = response_format
-        self.urls = URLs(debug)
+        self.urls = URLs(debug, response_format)
+
+    def __add_auth(self, params: dict) -> dict:
+        """Adds auth data to request params
+
+        Returns:
+            dict: Dictionary to be used as params on request
+        """
+        auth_dict = {"auth-userid": self.auth_userid, "api-key": self.api_key}
+
+        return {**params, **auth_dict}
+
+    def __to_format(self, response):
+        if self.response_format == "json":
+            return response.json()
+        else:
+            return ElementTree.fromstring(response.content)
+
+    def __get_data(self, url, params: dict = None):
+        full_params = self.__add_auth(params)
+        return self.__to_format(requests.get(url, full_params))
+
--- urls.py
+++ urls.py
@@ -4,14 +4,22 @@
 class URLs:
     """Stores all API URLs"""
 
-    prod_url = "https://httpapi.com/"
-    test_url = "https://test.httpapi.com/"
+    prod_url = "https://httpapi.com/api/"
+    test_url = "https://test.httpapi.com/api/"
 
     def __repr__(self) -> str:
-        return f"<{self.__class__.__name__}: {self}"
+        return f"<{self.__class__.__name__}: {self}>"
 
     def __str__(self) -> str:
         return self.base_url
 
-    def __init__(self, debug: bool = True) -> None:
+    def __init__(self, debug: bool = True, response_format: str = "json") -> None:
+        """Stores all API URLs
+
+        Args:
+            debug (bool, optional): Use the test or live API URLs. Defaults to True.
+            response_format (str, optional): Valid values are json or xml. Defaults to "json".
+        """
         self.base_url = self.test_url if debug else self.prod_url
+        self.format = response_format
+
